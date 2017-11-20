b67b19c10ae32024190c1efcc0e6068da1f4f8de
Dmitry Frank <mail@dmitryfrank.com>
2017-11-20 14:12:17 +0200

    Add docs

--- mjs_fs/api_ads1015.js
+++ mjs_fs/api_ads1015.js
@@ -13,51 +13,97 @@ let Adafruit_ADS1015 = {
   _glc: ffi('int mgos_ADS1015_getLastConversionResults(void *)'),
   _sg: ffi('void mgos_ADS1015_setGain(void *, int)'),
 
-  _proto: {
-    close: function() {
-      return Adafruit_ADS1015._close(this.ads);
-    },
-
-    begin: function() {
-      return Adafruit_ADS1015._begin(this.ads);
-    },
-	
-	readADC_SingleEnded: function(channel) {
-      return Adafruit_ADS1015._rse(this.ads, channel);
-    },
-	
-	readADC_Differential_0_1: function() {
-      return Adafruit_ADS1015._rd01(this.ads);
-    },
-	
-	readADC_Differential_2_3: function() {
-      return Adafruit_ADS1015._rd23(this.ads);
-    },
-	
-	startComparator_SingleEnded: function(channel, threshold) {
-      return Adafruit_ADS1015._sc(this.ads, channel, threshold);
-    },
-	
-	getLastConversionResults: function() {
-      return Adafruit_ADS1015._glc(this.ads);
-    },
-	
-	setGain: function(gain) {
-      return Adafruit_ADS1015._sg(this.ads,gain);
-    },
-	
-  },
-
+  // ## **`Adafruit_ADS1015.create(i2cAddress)`**
+  // Create an ADS1015 instance: an object with the methods described below.
+  // `i2cAddress` is an I2C address of the ADS1015.
   create: function(i2cAddress) {
     let obj = Object.create(Adafruit_ADS1015._proto);
     obj.ads = Adafruit_ADS1015._create(i2cAddress);
     return obj;
   },
-	
+
+  // ## **`Adafruit_ADS1115.create(i2cAddress)`**
+  // Create an ADS1115 instance: an object with the methods described below.
+  // `i2cAddress` is an I2C address of the ADS1115.
   create_ads1115: function(i2cAddress) {
     let obj = Object.create(Adafruit_ADS1015._proto);
     obj.ads = Adafruit_ADS1015._create_ads1115(i2cAddress);
     return obj;
   },
 
+  _proto: {
+    // ## **`myADS.close()`**
+    // Close an instance; no methods can be called on this instance after that.
+    // Return value: none.
+    close: function() {
+      return Adafruit_ADS1015._close(this.ads);
+    },
+
+    // ## **`myADS.begin()`**
+    // Set up the ADS1015/ADS1115 hardware
+    begin: function() {
+      return Adafruit_ADS1015._begin(this.ads);
+    },
+
+    // ## **`myADS.readADC_SingleEnded(channel)`**
+    // Get a single-ended ADC reading from the given `channel`.
+    readADC_SingleEnded: function(channel) {
+      return Adafruit_ADS1015._rse(this.ads, channel);
+    },
+
+    // ## **`myADS.readADC_Differential_0_1()`**
+    // Read the conversion results, measuring the difference between the P
+    // (AIN0) and N (AIN1) input. Returns a signed value since the difference
+    // can be either positive or negative.
+    readADC_Differential_0_1: function() {
+      return Adafruit_ADS1015._rd01(this.ads);
+    },
+
+    // ## **`myADS.readADC_Differential_0_1()`**
+    // Read the conversion results, measuring the difference between the P
+    // (AIN2) and N (AIN3) input. Returns a signed value since the difference
+    // can be either positive or negative.
+    readADC_Differential_2_3: function() {
+      return Adafruit_ADS1015._rd23(this.ads);
+    },
+
+    // ## **`myADS.startComparator_SingleEnded(channel, threshold)`**
+    // Set up the comparator to operate in basic mode, causing the ALERT/RDY
+    // pin to assert (go from high to low) when the ADC value exceeds the
+    // specified `threshold`.
+    // Return value: none.
+    startComparator_SingleEnded: function(channel, threshold) {
+      return Adafruit_ADS1015._sc(this.ads, channel, threshold);
+    },
+
+    // ## **`myADS.getLastConversionResults()`**
+    // In order to clear the comparator, we need to read the conversion
+    // results.  This function reads the last conversion results without
+    // changing the config value.
+    getLastConversionResults: function() {
+      return Adafruit_ADS1015._glc(this.ads);
+    },
+
+    // ## **`myADS.setGain(gain)`**
+    // Set the gain and input voltage range; one of the following:
+    //
+    // - `Adafruit_ADS1015.GAIN_TWOTHIRDS`
+    // - `Adafruit_ADS1015.GAIN_ONE`
+    // - `Adafruit_ADS1015.GAIN_TWO`
+    // - `Adafruit_ADS1015.GAIN_FOUR`
+    // - `Adafruit_ADS1015.GAIN_EIGHT`
+    // - `Adafruit_ADS1015.GAIN_SIXTEEN`
+    //
+    // Return value: none.
+    setGain: function(gain) {
+      return Adafruit_ADS1015._sg(this.ads,gain);
+    },
+  },
+
+  GAIN_TWOTHIRDS: 0x0000,
+  GAIN_ONE: 0x0200,
+  GAIN_TWO: 0x0400,
+  GAIN_FOUR: 0x0600,
+  GAIN_EIGHT: 0x0800,
+  GAIN_SIXTEEN: 0x0A00,
 }
--- src/mgos_arduino_Adafruit_ADS1015.h
+++ src/mgos_arduino_Adafruit_ADS1015.h
@@ -9,24 +9,71 @@ extern "C"
 {
 #endif
 
+/*
+ * Create an ADS1015 instance, with the given I2C address.
+ */
 Adafruit_ADS1015 *mgos_ADS1015_create(int i2cAddress);
 
+/*
+ * Create an ADS1115 instance, with the given I2C address.
+ */
 Adafruit_ADS1115 *mgos_ADS1115_create(int i2cAddress);
-  
+
+/*
+ * Close the given ADS1015/ADS1115 instance; no methods can be called on this
+ * instance after that.
+ */
 void mgos_ADS1015_close(Adafruit_ADS1015 *ads);
 
+/*
+ * Set up the ADS1015/ADS1115 instance; no methods can be called on this
+ * instance after that.
+ */
 void mgos_ADS1015_begin(Adafruit_ADS1015 *ads);
 
+/*
+ * Get a single-ended ADC reading from the given `channel`.
+ */
 int mgos_ADS1015_readADC_SingleEnded(Adafruit_ADS1015 *ads, int channel);
 
+/*
+ * Read the conversion results, measuring the difference between the P (AIN0)
+ * and N (AIN1) input. Returns a signed value since the difference can be
+ * either positive or negative.
+ */
 int mgos_ADS1015_readADC_Differential_0_1(Adafruit_ADS1015 *ads);
 
+/*
+ * Read the conversion results, measuring the difference between the P (AIN2)
+ * and N (AIN3) input. Returns a signed value since the difference can be
+ * either positive or negative.
+ */
 int mgos_ADS1015_readADC_Differential_2_3(Adafruit_ADS1015 *ads);
 
+/*
+ * Set up the comparator to operate in basic mode, causing the ALERT/RDY pin to
+ * assert (go from high to low) when the ADC value exceeds the specified
+ * `threshold`.
+ */
 void mgos_ADS1015_startComparator_SingleEnded(Adafruit_ADS1015 *ads, int channel, int threshold);
 
+/*
+ * In order to clear the comparator, we need to read the conversion results.
+ * This function reads the last conversion results without changing the config
+ * value.
+ */
 int mgos_ADS1015_getLastConversionResults(Adafruit_ADS1015 *ads);
 
+/*
+ * Set the gain and input voltage range; one of the following:
+ *
+// - `GAIN_TWOTHIRDS`
+// - `GAIN_ONE`
+// - `GAIN_TWO`
+// - `GAIN_FOUR`
+// - `GAIN_EIGHT`
+// - `GAIN_SIXTEEN`
+ */
 void mgos_ADS1015_setGain(Adafruit_ADS1015 *ads, adsGain_t gain);
 
 #ifdef __cplusplus
